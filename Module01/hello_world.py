6c4e1daf0124e89db8b1c46546c28a2567c44e41
soomin-e <52218216+soomin-e@users.noreply.github.com>
2019-06-25 18:50:49 -0400

    Hello_World_App Upload

---
+++ Module01/hello_world.py
@@ -0,0 +1,10 @@
+# Start Up Print Statements
+print('---------------------------------')
+print('             HELLO APP           ')
+print('---------------------------------')
+print()
+
+# Inputs
+print('Hello World')
+name = input('What is your name? ')
+print('Nice to meet you ' + name.title() + '.')
